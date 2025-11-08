d14d6e7abfbf4f7f7583780e0cc8ae1ddd45e976
Livia <livialutz385@gmail.com>
2025-11-08 19:25:05 -0300

    adicionando apps no requirements e no settings

--- site_receitas/site_receitas/settings.py
+++ site_receitas/site_receitas/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     #aplicacoes do projeto incluidas
     "receitas",
     "usuarios",
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
